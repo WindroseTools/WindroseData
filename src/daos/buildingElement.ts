3c40b69043d857b50a18c362b0a165e487bd7081
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-03-27 00:30:53 +0100

    update building elements with additional properties

--- src/daos/buildingElement.ts
+++ src/daos/buildingElement.ts
@@ -1,5 +1,5 @@
 import buildingElementData from "../../data/buildingElement.json";
-import { BEType } from "../types/BuildingElement";
+import { BEType, MountingRestriction, WorkingRestriction } from "../types/BuildingElement";
 import { MultiVersion, VersionKey } from "../versions";
 import { createVersionedRawStore, instantiateVersionedEntries, resolveVersionedRequirements } from "./helpers";
 import { RequirementEntry, RequirementUtils } from "./requirements";
@@ -9,6 +9,13 @@ type BuildingElementData<TRequired = number> = {
     type: BEType;
     limit?: number;
     required: Record<string, TRequired>;
+    revivalPoint?: boolean;
+    requiresBonfire?: boolean;
+    requiresShoreline?: boolean;
+    workingRestriction?: WorkingRestriction;
+    mountingRestriction?: MountingRestriction;
+    capacity?: number;
+    comfort?: number;
 };
 
 type BuildingElementRawData = BuildingElementData<number>;
@@ -20,12 +27,26 @@ export class BuildingElement {
     public type: BEType;
     public limit?: number;
     public required: Record<string, RequirementEntry>;
+    public revivalPoint?: boolean;
+    public requiresBonfire?: boolean;
+    public requiresShoreline?: boolean;
+    public workingRestriction?: WorkingRestriction;
+    public mountingRestriction?: MountingRestriction;
+    public capacity?: number;
+    public comfort?: number;
 
     constructor(id: string, data: BuildingElementResolvedData) {
         this.id = id;
         this.type = data.type;
         this.limit = data.limit;
         this.required = data.required;
+        this.revivalPoint = data.revivalPoint ?? false;
+        this.requiresBonfire = data.requiresBonfire ?? false;
+        this.requiresShoreline = data.requiresShoreline ?? false;
+        this.workingRestriction = data.workingRestriction ?? "none";
+        this.mountingRestriction = data.mountingRestriction ?? "none";
+        this.capacity = data.capacity;
+        this.comfort = data.comfort;
     }
 
     static loadBuildingElementsByVersion(): BuildingElementsByVersion {
--- src/types/BuildingElement.ts
+++ src/types/BuildingElement.ts
@@ -3,5 +3,10 @@ export const BETypes = [
     "pillar","beam","corner","window","door","stair","fence","trophy","seating","table","shelf","wardrobe","canopy","lamp","torch","dish","supply","shellDecoration",
     "pierDecoration","decoration","other"
 ] as const;
-
 export type BEType = (typeof BETypes)[number];
+
+export const WorkingRestrictions = ["none", "roofRequired", "placementOutside"] as const;
+export type WorkingRestriction = (typeof WorkingRestrictions)[number];
+
+export const MountingRestrictions = ["none", "wallOnly"] as const;
+export type MountingRestriction = (typeof MountingRestrictions)[number];
