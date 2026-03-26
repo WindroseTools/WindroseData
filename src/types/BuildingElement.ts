aaca074e59e2880868d68c3d867e67a397b7a830
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-03-26 14:45:43 +0100

    add bed, storage, foundation, floors, walls, roofs, pillars, beams, stairs, fences

--- src/types/BuildingElement.ts
+++ src/types/BuildingElement.ts
@@ -1,6 +1,6 @@
 export const BETypes = [
     "essential","crafting","cooking","personalEquipment","shipEquipment","alchemy","refining","bed","storage","label","foundation","floor","wall","roof",
-    "pillar","beam","window","door","stair","fence","trophy","seating","table","shelf","wardrobe","canopy","lamp","torch","dish","supply","shellDecoration",
+    "pillar","beam","corner","window","door","stair","fence","trophy","seating","table","shelf","wardrobe","canopy","lamp","torch","dish","supply","shellDecoration",
     "pierDecoration","decoration","other"
 ] as const;
 
