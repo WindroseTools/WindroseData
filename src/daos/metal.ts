34295415b78cf1b2a10566c0e4d9f850912982b8
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-04-05 00:42:55 +0200

    fix required export for metal

--- src/daos/metal.ts
+++ src/daos/metal.ts
@@ -2,7 +2,7 @@ import metalsData from "../../data/metal.json";
 import { Station } from "../types/Common";
 import { Rarity } from "../types/Rarity";
 import { MultiVersion, Version } from "../versions";
-import { createVersionedRawStore, instantiateVersionedEntries } from "./helpers";
+import { createVersionedRawStore, instantiateVersionedEntries, resolveVersionedRequirements } from "./helpers";
 import { RequirementEntry, RequirementUtils } from "./requirements";
 
 type MetalKey = keyof typeof metalsData;
@@ -41,13 +41,27 @@ export class Metal {
             (id, data) => {
                 const { required: _required, ...baseData } = data;
 
-                return new Metal(id, baseData);
+                return new Metal(id, { ...baseData, required: {} });
             },
         ) as MetalsByVersion;
 
         RequirementUtils.registerLookupContext({
             getMetal: (id, version) => metalsByVersion[version][id as MetalKey],
         });
+
+        const resolvers = [
+            ...RequirementUtils.createDefaultRequirementResolvers(),
+        ];
+
+        resolveVersionedRequirements(
+            rawByVersion,
+            metalsByVersion,
+            (data) => data.required,
+            (metal, required) => {
+                metal.required = required ?? {};
+            },
+            resolvers,
+        );
 
         return metalsByVersion;
     }
