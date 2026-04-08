daf3d5070e6d8045c6238fee23d8eae6e25e83ac
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-04-08 09:59:48 +0200

    add hut and hutFrame

--- src/types/BuildingElement.ts
+++ src/types/BuildingElement.ts
@@ -1,7 +1,7 @@
 export const BETypes = [
     "essential","crafting","cooking","personalEquipment","shipEquipment","alchemy","refining","bed","storage","label","foundation","floor","wall","roof",
     "pillar","beam","corner","window","door","stair","fence","trophy","seating","table","shelf","wardrobe","canopy","lamp","torch","dish","supply","shellDecoration",
-    "pierDecoration","decoration","other"
+    "pierDecoration","decoration","prebuiltStructure","other"
 ] as const;
 export type BEType = (typeof BETypes)[number];
 
