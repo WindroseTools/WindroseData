de684d898961bd9a3b8063c1629e249237c49cec
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-04-09 00:35:08 +0200

    add general Level, AttackLevel and DefenseLevel

---
+++ src/types/Level.ts
@@ -0,0 +1,14 @@
+export type Levels = Record<string, Level>;
+export type Level = {
+    required?: Record<string, number>;
+}
+
+export type AttackLevels = Record<string, AttackLevel>;
+export type AttackLevel = Level & {
+    attackScore: number;
+}
+
+export type DefenseLevels = Record<string, DefenseLevel>;
+export type DefenseLevel = Level & {
+    defenseScore: number;
+}
