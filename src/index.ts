26f85b17c4c666e43834b0609441747a04e3d47a
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-04-11 11:08:10 +0200

    added new exports to index

--- src/index.ts
+++ src/index.ts
@@ -11,9 +11,14 @@ export { Food, Foods } from "./daos/food";
 export { HullModification, HullModifications } from "./daos/hullModification";
 export { Item, Items } from "./daos/item";
 export { Medicine, Medicines } from "./daos/medicine";
+export { MeleeWeapon, MeleeWeapons } from "./daos/meleeWeapon";
 export { Metal, Metals } from "./daos/metal";
 export { Miscellanies, Miscellaneous } from "./daos/miscellaneous";
+export { Necklace, Necklaces } from "./daos/necklace";
+export { RangedWeapon, RangedWeapons } from "./daos/rangedWeapon";
 export { Resource, Resources } from "./daos/resource";
+export { Ring, Rings } from "./daos/ring";
+export { Set, Sets } from "./daos/set";
 export { Tool, Tools } from "./daos/tool";
 export { RequirementUtils } from "./daos/requirements";
 export { Versions, Version, MultiVersion, loadVersionedData } from "./versions";
@@ -27,10 +32,13 @@ export { populateEffectTranslation } from "./utils";
 export { Rarity } from "./types/Rarity";
 export type { RequirementEntry, ResolvedRequirement } from "./daos/requirements";
 export type { Effect, EffectType } from "./types/Effect";
-export type { Station, StatAffinity, StatAffinityClass, DamageType, Damages, Damage } from "./types/Common";
+export type { Station, StatAffinity, StatAffinityClass, StatDetail, StatDetails, DamageType, Damages, Damage } from "./types/Common";
+export type { Level, Levels, AttackLevel, AttackLevels, DefenseLevel, DefenseLevels } from "./types/Level";
+export type { MeleeWeaponType, RangedWeaponType, Ascension } from "./types/Weapon";
 
 // Constants
 export { Rarities } from "./types/Rarity";
-export { StatAffinities, StatAffinityClasses, DamageTypes } from "./types/Common";
+export { StatAffinities, StatAffinityClasses, DamageTypes, StatAffinityDamageTypeMap } from "./types/Common";
+export { MeleeWeaponTypes, RangedWeaponTypes } from "./types/Weapon";
 
 export { Languages, LanguageData } from "./languages";
