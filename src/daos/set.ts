8a05070ee78e3389a9d88ee396a57ccd4068fc99
herby2212 <12448284+herby2212@users.noreply.github.com>
2026-04-19 11:38:56 +0200

    add set type

--- src/daos/set.ts
+++ src/daos/set.ts
@@ -1,11 +1,13 @@
 import setsData from "../../data/set.json";
 import { Effect } from "../types/Effect";
+import { SetType } from "../types/Set";
 import { MultiVersion, Version } from "../versions";
 import { createVersionedRawStore, instantiateVersionedEntries } from "./helpers";
 import { RequirementUtils } from "./requirements";
 
 type SetKey = keyof typeof setsData;
 type SetData = {
+    type: SetType;
     pieces: string[];
     pieceBonuses: Record<string, Effect[]>;
 };
@@ -14,12 +16,14 @@ type SetsByVersion = MultiVersion<SetKey, Set>;
 
 export class Set {
     public id: string;
+    public type: SetType;
     public dataType: "set";
     public pieces: string[];
     public pieceBonuses: Record<string, Effect[]>;
 
     constructor(id: string, data: SetData) {
         this.id = id;
+        this.type = data.type;
         this.dataType = "set";
         this.pieces = data.pieces;
         this.pieceBonuses = data.pieceBonuses;
